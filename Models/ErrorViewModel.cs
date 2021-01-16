f0dd58124be42e9dcae8667462821cd6c93f3ae1
RodrigoGigena <rodrigotomasgigena@outlook.com>
2021-01-15 23:11:53 -0300

    Mi primer proyecto en GIT

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mi_proyecto.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
